a58c2178cfe8b1e9bb25aef8fd3927bef9aaaaa2
anujgh <anujphone7@gmail.com>
2024-07-01 12:36:18 +0530

    remove redis code

--- actions/userAction.js
+++ actions/userAction.js
@@ -1,19 +1,19 @@
-const redisClient = require('../helpers/redisConfig')
+// const redisClient = require('../helpers/redisConfig')
 const axios = require('axios')
 const getUserList = async () => {
   console.log('called---')
   let users
-  users = await redisClient.get('users')
+ /*  users = await redisClient.get('users')
   users = JSON.parse(users)
   if(users){
     return users
-  }
+  } */
 
   users = await axios.get('https://jsonplaceholder.typicode.com/users')
   users = users.data
   // users = await fetch('https://jsonplaceholder.typicode.com/users')
   console.log('fetching the data...')
-  redisClient.set('users', JSON.stringify(users))
+  // redisClient.set('users', JSON.stringify(users))
   return users;
 }
 
